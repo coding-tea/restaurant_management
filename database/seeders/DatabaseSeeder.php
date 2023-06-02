512a025d9bb7cbd0d1f969ed1bbe9bc1cc6fc5b8
digital opium <test@ismail.com>
2023-06-02 16:10:51 +0100

    add the seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Serveur;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            UserSeeder::class,
+            CategorieSeeder::class,
+            PlatSeeder::class,
+            ComposantSeeder::class,
+          ComposantPlatSeeder::class,
+            ServeurSeeder::class,
+            CommandeSeeder::class,
+            CommandePlatSeeder::class
+        ]);
     }
 }
