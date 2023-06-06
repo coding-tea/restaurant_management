82b55e02cbad3500182918bdabd9bf24a4043d7a
digital opium <test@ismail.com>
2023-06-06 21:36:39 +0100

    get back my routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategorieController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,5 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/', [ CategorieController::class, 'index' ])->name('menu');
+Route::get('/menu', [ CategorieController::class, 'index' ])->name('menu');
+Route::get('/plats/{id}', [ CategorieController::class, 'plats' ])->name('categorie-plats');
 
 require __DIR__.'/auth.php';
